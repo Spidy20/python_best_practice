60fdd3a9af16cef29de01eda8d0f0d9184ea8bfa
Spidy <kushalbhavsar58@gmail.com>
2020-05-16 19:05:49 +0530

    Facial Landmark Detection

---
+++ face_landmarks.py
@@ -0,0 +1,27 @@
+import cv2
+import dlib
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        landmarks = predictor(gray, face)
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 4, (0, 255, 0), -1)
+    cv2.imshow("fACIAL lANDMARK dETECTION", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cv2.destroyWindow()
